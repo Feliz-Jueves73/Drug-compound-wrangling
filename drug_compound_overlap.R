755a0811933e13def4afc03b0a540ffc1c519910
Joseph <joextreme73@gmail.com>
2024-07-29 13:20:21 -0500

    Update drug_compound_overlap.R

--- drug_compound_overlap.R
+++ drug_compound_overlap.R
@@ -1,7 +1,7 @@
 library(readxl)
 library(tidyverse)
-sheet1 <- read_excel("C:/Users/rolli161/Downloads/AVIDD-EXONUCLEASE_LASSA_CONF-161_PlateMap.xlsx")
-sheet2 <- read_excel("C:/Users/rolli161/Downloads/AVIDD-EXONUCLEASE_SARS2_CONF-162_PlateMap.xlsx")
+sheet1 <- read_excel("C:Users/lorem_ipsum_")
+sheet2 <- read_excel("C:/Users/lorem_ipsum")
 df1 <- sheet1$`Sample ID`
 df2 <- sheet2$`Sample ID`
 overlap <- intersect(df1, df2)
